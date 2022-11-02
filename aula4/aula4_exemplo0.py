fe0fd801c6b74d86a08772b0d4c303d87eb9080b
Alexandre Rodrigues <ale.rodrigues@outlook.com>
2022-11-02 14:35:54 -0300

    Condições IF, ELIF e ELSE - Aula 4

---
+++ aula4/aula4_exemplo0.py
@@ -0,0 +1,12 @@
+# Condições IF, ELIF e ELSE - Aula 4
+
+i = input("entre com o valor ")
+i = int(i)
+
+if i < 1:
+    print("menor que 1")
+elif i  > 1:
+    print("maior que 1")
+else:
+    print("igual a 1")
+
